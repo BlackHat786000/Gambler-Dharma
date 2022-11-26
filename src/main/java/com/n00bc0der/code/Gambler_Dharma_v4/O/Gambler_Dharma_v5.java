c07746defc629fc70b16a47297b6fb5a6712417a
Udit Yadav <yadavudit786@gmail.com>
2022-11-26 11:14:53 +0530

    Bug fixes & enhancements

--- src/main/java/com/n00bc0der/code/Gambler_Dharma_v4/O/Gambler_Dharma_v5.java
+++ src/main/java/com/n00bc0der/code/Gambler_Dharma_v4/O/Gambler_Dharma_v5.java
@@ -816,19 +816,19 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl)))) {
 				ASC_SUB = ASC_SUB + 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl)))) {
 				ASC_SUB = ASC_SUB - 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl)) {
@@ -836,7 +836,7 @@ public class Gambler_Dharma_v5 {
 				} else if (Iterables.get(house_view.get(i), 3).contains(asc_csl)) {
 					ASC_SUB = ASC_SUB + 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl)) {
@@ -849,9 +849,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(asc_csl))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(asc_csl))) {
 						ASC_SUB = ASC_SUB + cnt;
-					} else if ((i == 5 || i == 12) && (s.contains(asc_csl))) {
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12) && (s.contains(asc_csl))) {
 						ASC_SUB = ASC_SUB - cnt;
 					}
 					cnt = cnt - 1;
@@ -864,24 +864,24 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl))))) {
 				DSC_SUB = DSC_SUB - 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl))))) {
 				DSC_SUB = DSC_SUB + 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl)) {
 					DSC_SUB = DSC_SUB - 4;
 				} else if (Iterables.get(house_view.get(i), 3).contains(dsc_csl)) {
 					DSC_SUB = DSC_SUB - 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl)) {
 					DSC_SUB = DSC_SUB + 4;
@@ -893,9 +893,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(dsc_csl))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(dsc_csl))) {
 						DSC_SUB = DSC_SUB - cnt;
-					} else if ((i == 5 || i == 12) && (s.contains(dsc_csl))) {
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12) && (s.contains(dsc_csl))) {
 						DSC_SUB = DSC_SUB + cnt;
 					}
 					cnt = cnt - 1;
@@ -932,7 +932,7 @@ public class Gambler_Dharma_v5 {
 //									  ADVANCED
 //------------------------------------------------------------------------------------------------------------------
 
-		System.out.println("\n\nADVANCED : Using Star and Sub of 1st & 7th CSL");
+		System.out.println("\n\nADVANCED : Using Star and Sub of 6th & 12th CSL");
 		System.out.println("==============================================\n");
 
 		int asc_csl_star = Iterables.get(planets.get(planets_id.get(asc_csl)), 4);
@@ -959,19 +959,19 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl_stars)))) {
 				ASC_SUB_STAR = ASC_SUB_STAR + 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl_stars)))) {
 				ASC_SUB_STAR = ASC_SUB_STAR - 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl_stars)) {
@@ -979,7 +979,7 @@ public class Gambler_Dharma_v5 {
 				} else if (Iterables.get(house_view.get(i), 3).contains(asc_csl_stars)) {
 					ASC_SUB_STAR = ASC_SUB_STAR + 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl_stars)) {
@@ -992,9 +992,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(asc_csl_stars))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(asc_csl_stars))) {
 						ASC_SUB_STAR = ASC_SUB_STAR + cnt;
-					} else if ((i == 5 || i == 12)
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 							&& (s.contains(asc_csl_stars))) {
 						ASC_SUB_STAR = ASC_SUB_STAR - cnt;
 					}
@@ -1006,24 +1006,24 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl_stars))))) {
 				DSC_SUB_STAR = DSC_SUB_STAR - 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl_stars))))) {
 				DSC_SUB_STAR = DSC_SUB_STAR + 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl_stars)) {
 					DSC_SUB_STAR = DSC_SUB_STAR - 4;
 				} else if (Iterables.get(house_view.get(i), 3).contains(dsc_csl_stars)) {
 					DSC_SUB_STAR = DSC_SUB_STAR - 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl_stars)) {
 					DSC_SUB_STAR = DSC_SUB_STAR + 4;
@@ -1035,9 +1035,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(dsc_csl_stars))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(dsc_csl_stars))) {
 						DSC_SUB_STAR = DSC_SUB_STAR - cnt;
-					} else if ((i == 5 || i == 12)
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 							&& (s.contains(dsc_csl_stars))) {
 						DSC_SUB_STAR = DSC_SUB_STAR + cnt;
 					}
@@ -1070,19 +1070,19 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl_subs)))) {
 				ASC_SUB_SUB = ASC_SUB_SUB + 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals(""))
 							&& (Iterables.get(house_view.get(i), 2).equals(""))
 							&& (Iterables.get(house_view.get(i), 3).contains(asc_csl_subs)))) {
 				ASC_SUB_SUB = ASC_SUB_SUB - 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl_subs)) {
@@ -1090,7 +1090,7 @@ public class Gambler_Dharma_v5 {
 				} else if (Iterables.get(house_view.get(i), 3).contains(asc_csl_subs)) {
 					ASC_SUB_SUB = ASC_SUB_SUB + 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0).equals(""))
 							&& (Iterables.get(house_view.get(i), 1).equals("")))) {
 				if (Iterables.get(house_view.get(i), 2).contains(asc_csl_subs)) {
@@ -1103,9 +1103,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(asc_csl_subs))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(asc_csl_subs))) {
 						ASC_SUB_SUB = ASC_SUB_SUB + cnt;
-					} else if ((i == 5 || i == 12)
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 							&& (s.contains(asc_csl_subs))) {
 						ASC_SUB_SUB = ASC_SUB_SUB - cnt;
 					}
@@ -1124,24 +1124,24 @@ public class Gambler_Dharma_v5 {
 
 		for (int i = 1; i <= 12; i++) {
 
-			if ((i == 6 || i == 11)
+			if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl_subs))))) {
 				DSC_SUB_SUB = DSC_SUB_SUB - 4;
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == "")
 							&& (Iterables.get(house_view.get(i), 2) == ""
 									&& (Iterables.get(house_view.get(i), 3).contains(dsc_csl_subs))))) {
 				DSC_SUB_SUB = DSC_SUB_SUB + 4;
-			} else if ((i == 6 || i == 11)
+			} else if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl_subs)) {
 					DSC_SUB_SUB = DSC_SUB_SUB - 4;
 				} else if (Iterables.get(house_view.get(i), 3).contains(dsc_csl_subs)) {
 					DSC_SUB_SUB = DSC_SUB_SUB - 2;
 				}
-			} else if ((i == 5 || i == 12)
+			} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 					&& ((Iterables.get(house_view.get(i), 0) == "") && (Iterables.get(house_view.get(i), 1) == ""))) {
 				if (Iterables.get(house_view.get(i), 2).contains(dsc_csl_subs)) {
 					DSC_SUB_SUB = DSC_SUB_SUB + 4;
@@ -1153,9 +1153,9 @@ public class Gambler_Dharma_v5 {
 				for (int j = 0; j <= 3; j++) {
 					String s = Iterables.get(house_view.get(i), j);
 
-					if ((i == 6 || i == 11) && (s.contains(dsc_csl_subs))) {
+					if ((i == 1 || i == 2 || i == 3 || i == 6 || i == 10 || i == 11) && (s.contains(dsc_csl_subs))) {
 						DSC_SUB_SUB = DSC_SUB_SUB - cnt;
-					} else if ((i == 5 || i == 12)
+					} else if ((i == 7 || i == 8 || i == 9 || i == 4 || i == 5 || i == 12)
 							&& (s.contains(dsc_csl_subs))) {
 						DSC_SUB_SUB = DSC_SUB_SUB + cnt;
 					}
